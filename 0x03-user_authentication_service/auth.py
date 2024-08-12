2e0e81f1a8a4feb84b6f223cbda4825b24541a83
Iamouma <stephenouma452@gmail.com>
2024-08-12 08:33:23 +0000

    Implementing a register method by registering a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,3 +15,32 @@ def _hash_password(password: str) -> bytes:
     Hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def _generate_uuid() -> str:
+    """
+    Returns a string representation of a new UUID
+    """
+    UUID = uuid4()
+    return str(UUID)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
+        else:
+            raise ValueError(f"User {email} already exists")
+
+
