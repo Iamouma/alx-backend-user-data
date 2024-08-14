cf0da99018189a94edbf2075432995af8760666b
Iamouma <stephenouma452@gmail.com>
2024-08-14 08:06:10 +0000

    Authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Hash password
-"""
+"""Hash password"""
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
@@ -11,16 +9,13 @@ from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
-    """
-    Hash password
+    """Hash a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def _generate_uuid() -> str:
-    """
-    Returns a string representation of a new UUID
-    """
+    """Returns a string representation of a new UUID"""
     UUID = uuid4()
     return str(UUID)
 
@@ -33,9 +28,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """
-        Register user
-        """
+        """Register user"""
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
@@ -45,9 +38,7 @@ class Auth:
             raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
-        """
-        If password is valid returns true, else. false
-        """
+        """If password is valid returns true, else, false"""
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
@@ -62,9 +53,7 @@ class Auth:
         return False
 
     def create_session(self, email: str) -> str:
-        """
-        Returns session ID for a user
-        """
+        """ Returns session ID for a user """
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
@@ -75,3 +64,56 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """It takes a single session_id string argument
+        Returns a string or None
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Updates the corresponding user's session ID to None"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+
+        self._db.update_user(user.id, session_id=None)
+
+        return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset password token if user exists"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Uses reset token to validate update of users password"""
+        if reset_token is None or password is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
